b59d9817b385b975e815b28a75e6bbaaea7a6bcb
enatrage <103586619+enatrage@users.noreply.github.com>
2025-10-05 18:55:46 +0300

    intermediary push
    intermediary push, to move code from my desktop to laptop

--- defs/diffusion/diffusion.py
+++ defs/diffusion/diffusion.py
@@ -1,3 +1,26 @@
 """
 Define the diffusion class here
 """
+
+class cond_diffusion:
+
+    def __init__(self, epsilon, scheduler):
+        self.epsilon = epsilon # Take in the epsilon network
+        self.scheduler = scheduler # Take in the noise scheduler
+
+    def _gather_stats(self, t):
+        """
+        Gathers relevant stats from the scheduler.
+        """
+
+
+
+    def train(self, x_0, ab):
+        """
+        Trains the epsilon network, takes in some initial x_0 and some abundances related to it.
+        """
+
+    def sample(self, x_T, ab):
+        """
+        Samples from the 
+        """
--- defs/diffusion/epsilon.py
+++ defs/diffusion/epsilon.py
@@ -1,3 +1,8 @@
 """
 Define different epsilons over here
 """
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
