5d3ad36badbd1b4b4193da57900d437dfde0387c
enatrage <103586619+enatrage@users.noreply.github.com>
2025-10-12 17:33:29 +0300

    intermediary push
    intermediary push, checkpoint purposes

--- defs/training_defs/loss.py
+++ defs/training_defs/loss.py
@@ -1,24 +1,35 @@
 import torch.nn.functional as F
 import torch
 
-def loss_mse_sam(x0: torch.tensor, x0_hat: torch.tensor, ratio: float):
-    """
-    Loss function to calculate the MSE + SAM from a reconstructed spectrum x0 (actual) and x0_hat (reconstructed). Assumes horiz inputs.
+class loss_mse_sam:
 
-    Parameters
-        - x0 (torch.tensor), Actual spectrum x0
-        - x0_hat (torch.tensor), The reconstructed x0
-        - Ratio (float), Ratio is a hyperparam between MSE and SAM
+    def __init__(self, ratio: float):
 
-    Returns
-        - Loss (float), MSE + Lambda * SAM
-    """
+        """
+        Loss function to calculate the MSE + SAM from a reconstructed spectrum x0 (actual) and x0_hat (reconstructed). Assumes horiz inputs.
+            - Ratio (float), Ratio is a hyperparam between MSE and SAM
 
-    # The MSE loss, straightforward
-    loss_mse = F.mse_loss(x0_hat, x0)
+        
+        """
+        self.ratio = ratio
 
-    # The SAM loss, compute cos similarity first (which is the actual SAM formula, without arccos)
-    cos_sim = F.cosine_similarity(x0_hat, x0, dim=1)
-    loss_sam = torch.acos(torch.clamp(cos_sim, -1.0, 1.0))
+    def calc(self, x0: torch.tensor, x0_hat: torch.tensor):
+        """
+        Calculates the loss
 
-    return loss_mse + ratio * loss_sam # Return the total loss
+        Parameters
+            - x0 (torch.tensor), Actual spectrum x0
+            - x0_hat (torch.tensor), The reconstructed x0
+
+        Returns
+            - Loss (float), MSE + Lambda * SAM
+        """
+
+        # The MSE loss, straightforward
+        loss_mse = F.mse_loss(x0_hat, x0)
+
+        # The SAM loss, compute cos similarity first (which is the actual SAM formula, without arccos)
+        cos_sim = F.cosine_similarity(x0_hat, x0, dim=1)
+        loss_sam = torch.acos(torch.clamp(cos_sim, -1.0, 1.0)).mean()
+
+        return loss_mse + self.ratio * loss_sam # Return the total loss
--- defs/training_defs/training_defs.py
+++ defs/training_defs/training_defs.py
@@ -88,44 +88,46 @@ def get_dataloaders(ds: HyperSpectralDataset, cfg_loader: dict):
     Parameters:
         - ds (HyperSpectralDataset): The entire dataset as a HyperSpectralDataset class.
         - cfg_loader (dict):
-            - 'validate' (int): How many samples to be used to validate at the end
+            - 'test' (int): How many samples to be used to test at the end
             - 't_batch' (int): Train with batches of what size, every epoch
-            - 'test' (int): How many samples to test on each epoch
+            - 'validate' (int): How many samples to validate on each epoch
             - 'epoch' (int): How many epochs in total
     """
 
     # Take in the values from the dict
-    n_validate = cfg_loader.get('validate', 23)
-    n_test = cfg_loader.get('test', 4)
+    n_test = cfg_loader.get('test', 23)
+    n_validate = cfg_loader.get('validate', 4)
     n_t_batch = cfg_loader.get('t_batch', 1)
     n_epoch = cfg_loader.get('epoch', 50)
 
     # Infer the amount of n_train, ensure completeness
-    n_train = int(len(ds) - n_epoch*n_test - n_validate)
+    n_train = int(len(ds) - n_epoch*n_validate - n_test)
     assert n_train % n_epoch == 0, f"Something is wrong with the sample allocation, n_train: {n_train} is not divisible by n_epoch: {n_epoch}"
     assert n_train/n_epoch % n_t_batch, f"Train sample per epoch: {n_train/n_epoch}, must be divisible by batch size in train: {n_t_batch}"
 
     # Separate the dataset into validate and temporary
-    ds_validate, ds_temp = random_split(ds, [n_validate, n_train + n_test*n_epoch]) 
+    ds_test, ds_temp = random_split(ds, [n_test, n_train + n_validate*n_epoch]) 
 
     # Separate the temp dataset into train and test
-    ds_train, ds_test = random_split(ds_temp, [n_train, n_epoch*n_test])
+    ds_train, ds_validate = random_split(ds_temp, [n_train, n_epoch*n_validate])
 
-    return DataLoader(ds_train, batch_size=n_t_batch, shuffle=True), DataLoader(ds_test, batch_size=1, shuffle= True), DataLoader(ds_validate, batch_size=1, shuffle=True)
+    return DataLoader(ds_train, batch_size=n_t_batch, shuffle=True), DataLoader(ds_validate, batch_size=1, shuffle= True), DataLoader(ds_test, batch_size=1, shuffle=True)
 
-def train_diffusion(cfg_train: dict, loss, cond_diffusion, data_handle: str=None):
+def train_diffusion(cfg_train: dict, cond_diffusion, loss, optimizer: torch.optim, data_handle: str=None):
 
     """
     The function to train and validate the models, takes in:
         - cfg_train (dict): 
             - cfg_loader (dict):
-                - 'validate' (int): How many samples to be used to validate at the end
+                - 'test' (int): How many samples to be used to test at the end
                 - 't_batch' (int): How many train batches in total, to be trained on
-                - 'test' (int): How many samples to test on each epoch
-                - epoch' (int): How many epochs in total
+                - 'validate' (int): How many samples to validate on each epoch
+                - 'epoch' (int): How many epochs in total
             - 'range' (list): Spectral range [lower, upper]
-        - loss (fn): Loss function, must have MSE and SAM at the very least
+            - 'device' : The device being used
         - cond_diffusion (class): The conditional diffusion class, already initialized
+        - loss (class): Loss function class, must have MSE and SAM at the very least
+        - optimizer (torch.optim): The optimizer to use
         - data_handle (str): The path handle to the dataset
     """
 
@@ -141,21 +143,88 @@ def train_diffusion(cfg_train: dict, loss, cond_diffusion, data_handle: str=None
 
     # Dataload the giant dataset into train, test, validation datasets
     cfg_loader = cfg_train.get(
-        'cfg_loader', {'validate': 23, 't_batch': 1, 'test': 4, 'epoch': 50}
+        'cfg_loader', {'test': 23, 't_batch': 1, 'validate': 4, 'epoch': 50}
     ) # Get the configs for the loader first
 
     # Get the dataloaded datasets
-    ds_train, ds_test, ds_validation = get_dataloaders(ds, cfg_loader=cfg_loader)
+    ds_train, ds_validation , ds_test= get_dataloaders(ds, cfg_loader=cfg_loader)
 
-    n_validate = cfg_loader.get('validate', 23)
+    n_validate = cfg_loader.get('test', 23)
     n_epoch = cfg_loader.get('epoch', 50)
     n_t_batch = cfg_loader.get('t_batch', 1)
-    n_test = cfg_loader.get('test', 4)
+    n_test = cfg_loader.get('validate', 4)
+
+    # Infer the amount of n_train, ensure completeness
+    n_train = int(len(ds) - n_epoch*n_validate - n_test)
+    n_train_per_epoch = n_train // n_epoch
 
     for epoch in range(1, n_epoch+1):
 
+        # Set the epsilon in training mode
         cond_diffusion.epsilon.train()
+        total_train_loss = 0 # Accumulate total train loss per epoch
+        
+        for _ in range(n_train_per_epoch):
+
+            # Get the next batch
+            try:
+                batch = next(ds_train)
+
+            except StopIteration:
+                # If the dataset is exhausted, reset the iterator for the next epoch
+                raise StopIteration("The training dataset has been exhausted.")
+
+            x0, abundances, name, orig_index = batch['spectrum'], batch['abundances'], batch['names'], batch['orig_index'] # Unpack the batch data
+            x0.to(cfg_train['device']), abundances.to(cfg_train['device']) # Move them to the device
+
+            # Zero all the grads
+            optimizer.zero_grad()
+
+            # Add noise, denoise, and get x0_hat predictions
+            x0_hat = cond_diffusion.training_procedure(x0, abundances) 
+
+            # Calculate the loss based on the reconstructed predictions and the actual spectra
+            total_loss = loss.calc(x0, x0_hat) 
+
+            # Take the backprop and take a step
+            total_loss.backward()
+            optimizer.step()
+
+            total_train_loss += loss.item()
+
+        print(f"Epoch {epoch + 1} | Average Training Loss: {total_train_loss:.4f}") # Training for this epoch finished, print the results
+
+        cond_diffusion.epsilon.eval() # Set the model in eval mode
+        total_val_loss = 0 # Accumulate total validate loss
+
+        with torch.no_grad():
+
+            for _ in range(n_validate):
+
+                try:
+                    # Fetch the validation batch
+                    batch = next(val_loader)
+                except StopIteration:
+                    val_loader = iter(DataLoader(ds_validation, batch_size=1, shuffle=False))
+                    batch = next(val_loader)
+                
+                x0, abundances, name, orig_index = batch['spectrum'], batch['abundances'], batch['names'], batch['orig_index'] # Unpack the batch data
+                x0.to(cfg_train['device']), abundances.to(cfg_train['device']) # Move them to the device
+
+                # Get some x0_preds, conditional on the abundances themselves
+                x0_pred = cond_diffusion.sample(ab= abundances)
+
+                
+
+
+
+
+
+
+
+
 
         # The rest will be pushed soon
+
 
     
