06800cf6d8abddfecf7812f7db97c2167fa4902a
enatrage <103586619+enatrage@users.noreply.github.com>
2025-10-25 14:21:04 +0300

    minor changes to training_defs

--- defs/diffusion/epsilon.py
+++ defs/diffusion/epsilon.py
@@ -127,4 +127,11 @@ class Epsilon_MLP(nn.Module):
 
         return self.denoiser(h)
     
-    
+def get_n_params(model):
+    pp=0
+    for p in list(model.parameters()):
+        nn=1
+        for s in list(p.size()):
+            nn = nn*s
+        pp += nn
+    return pp
