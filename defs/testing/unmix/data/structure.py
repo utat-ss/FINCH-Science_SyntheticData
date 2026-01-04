49ac34a0458d3ea40fdb7469c717676d58a85f9c
enatrage <103586619+enatrage@users.noreply.github.com>
2026-01-04 13:45:59 -0500

    reworking testing workflow
    - created a master script
    - majorly reworked unmixing metric scripts

--- defs/testing/unmix/data/structure.py
+++ defs/testing/unmix/data/structure.py
@@ -10,88 +10,11 @@ What we want to do is:
 3 - set the ksi_test = Subsection(psi_2), i.e. it comes from a subsection of the real data that was not used for synthesizer train
 """
 
-
-
 import torch
 import pandas as pd
 
 from torch.utils.data import Dataset, DataLoader
 from typing import Iterator
-
-def get_vals(data_handle:(str), spec_range:(list[int]), cfg_normalize:(dict)):
-
-    """
-    The function to get all the datasets.
-
-    Args:
-        data_handle (str): The str path to the data
-        spec_range (list[int]): Inclusive range of the spectral range given as list of ints
-        cfg_normalize (dict): The dict to normalize the data
-
-    Returns:
-        spectra_tensor (torch.Tensor): Spectral val tensor
-        abundances_tensor (torch.Tensor): Abundances tensor
-        names (list): A list of all the spectral 
-        indices (list): A list of the actual indices in a csv file
-        norm_out (dict): Details related to how the dataset was normalized
-    """
-
-    # Unpack the dicts
-
-    # Get the dataframe given the handle
-    df = pd.read_csv(data_handle)
-
-    # Get the spectral vals given the spectral range
-    spectral_cols = [str(w) for w in range(spec_range[0], spec_range[1]+1, 10)]
-    spectra = df[spectral_cols].values.astype("float32")
-    spectra_tensor = torch.from_numpy(spectra)
-    del spectra, spectral_cols
-
-    norm_type = cfg_normalize['norm_type']
-    if norm_type=='classic': # Classic, very blunt normalization
-        spectra_tensor = (2 * spectra_tensor) - 1 # Bluntly normalize it
-        norm_cfg = {
-            'norm_type': norm_type
-        }
-    elif norm_type=='dynamic': # Dynamically scales using the max of the dataset
-        max_vals = torch.max(spectra_tensor)
-        min_vals = torch.min(spectra_tensor)
-        spectra_tensor = 2 * ((spectra_tensor - min_vals) / (max_vals - min_vals)) - 1 # Sets the dataset [-1,1] filling all regions
-        norm_out = {
-            'norm_type': norm_type,
-            'max_vals': max_vals,
-            'min_vals': min_vals
-        }
-    elif norm_type =='log': # Dynamically scales using log and min and max of the dataset
-        eps = 1e-6
-        spectra_tensor = torch.log(spectra_tensor + eps)
-        max_vals = torch.max(spectra_tensor)
-        min_vals = torch.min(spectra_tensor)
-        spectra_tensor = 2 * ((spectra_tensor - min_vals) / (max_vals - min_vals)) - 1
-        norm_out = {
-            'norm_type': norm_type,
-            'max_vals': max_vals,
-            'min_vals': min_vals,
-            'eps': eps
-        }
-    elif norm_type=='none':
-        norm_out = {
-            'norm_type': norm_type
-        }
-
-    # Get the abundances
-    abundances = df[["gv_fraction","npv_fraction","soil_fraction"]].values.astype("float32")
-    abundances_tensor = torch.from_numpy(abundances)
-    del abundances
-
-    # Get the spectral names
-    names = df['Spectra'].to_list()
-
-    # Get the original indices
-    indices = range(len(names))
-
-    return spectra_tensor, abundances_tensor, names, indices, norm_out
-
 
 class HyperSpectralDataset(Dataset):
     """
@@ -100,10 +23,10 @@ class HyperSpectralDataset(Dataset):
     Args:
         spectra (torch.Tensor): Tensor of all the spectra, as a tensor
         abundances (torch.Tensor): Abundances of all the spectra, as a tensor
-        names (list): Names of the spectra, as a list
-        indices (list): Indices in order, as a list
+        names (list[str]): Names of the spectra, as a list
+        indices (list[int]): Indices in order, as a list
     """
-    def __init__(self, spectra:(torch.Tensor), abundances:(torch.Tensor), names:(list), indices:(list)):
+    def __init__(self, spectra:(torch.Tensor), abundances:(torch.Tensor), names:(list[str]), indices:(list[int])):
 
         self.spectra = spectra
         self.abundances = abundances
@@ -121,58 +44,13 @@ class HyperSpectralDataset(Dataset):
             'names': self.names[idx],
             'orig_index': self.indices[idx]
         }
-    
-
-def get_dataloader(ds:(HyperSpectralDataset), cfg_loader:(dict), seed:(int), ds_type:(str), ds_use:(str)) -> DataLoader:
-    """
-    Gets the dataloaders for train, test, validate datasets.
-
-    Args:
-        ds (HyperSpectralDataset): The entire dataset as a HyperSpectralDataset class.
-        cfg_loader (dict):
-            n_epoch (int): How many epochs in total
-            n_batch_epoch (int): How many batches in total, in a single epoch
-            batch_size (int): Size of each batch
-        seed (int): Seed for random shuffle/split
-        ds_type (dict): Type of the data, either 'synthesized' or 'real'
-        ds_use (dict): The use of the dataset, either 'train', 'val', 'test'
-    
-    Returns:
-        dataloaders (DataLoader): The dataloader
-    """
-
-    # Get the RNG, for reproducibility
-    generator = torch.Generator()
-    generator.manual_seed(seed)
-
-    if (ds_type == 'synthesized' or ds_type == 'real') and ds_use == 'train':
-
-        # Unpack the dict
-        n_epoch = cfg_loader['n_epoch']
-        n_batch_epoch = cfg_loader['n_batch_epoch']
-        batch_size = cfg_loader['batch_size']
-
-        assert len(ds) >= n_epoch*n_batch_epoch*batch_size
-
-        return DataLoader(ds, batch_size=batch_size, shuffle=True, generator=generator, drop_last=True)
-    
-    
-    if ds_type == 'real' and (ds_use == 'val' or ds_use == 'test'):
-
-        # No need to unpack anything
-
-        return DataLoader(ds, batch_size=len(ds), shuffle=True, generator=generator)
-    
-    else:
-        raise ValueError(f"Unknown combination of dataset use: {ds_use} and dataset type: {ds_type}")
-
 
 def get_inf_iterator(dataloader:(DataLoader)) -> Iterator:
     """
     Gets the infinite iterator, it can be infinitely iterated through.
 
     Args:
-        dataloaders (DataLoader): An already prepared dataloader
+        dataloader (DataLoader): An already prepared dataloader
     
     Returns:
         cycle(dataloader): An infinitely iterable dataloader
--- defs/testing/unmix/script.py
+++ defs/testing/unmix/script.py
@@ -6,6 +6,13 @@ import yaml
 
 import torch
 
+from .loaders.loaders import *
+from .critic_train.train import train_critic
+from .data.data import get_data
+
+import traceback
+import logging
+
 def cfg_from_args():
     """
     Function to get args from the .yaml args given in the script run code
@@ -31,15 +38,16 @@ def parse_cfg_dict(cfg_run:(dict)):
         cfg_run (dict): The cfg_run dict
 
     Returns:
-        cfg_data (dict): cfg dict to prepare the real and synthesized datasets
+        cfg_import (dict): cfg dict related to imported data
+        cfg_loader (dict): cfg dict related to preparing the loaders
         cfg_train (dict): cfg dict for the training of the critic model
         cfg_export (dict): cfg dict to export things to wandb
-
         cfg_critic_setup (dict): cfg dict to load the critic
         cfg_critictrain_setup (dict): cfg dict to handle the optimizer, lr_scheduler, and loss functions of the critic
     """
     # Config parsing
-    cfg_data = cfg_run['cfg_data']
+    cfg_import = cfg_run['cfg_import']
+    cfg_loader = cfg_run['cfg_loader']
     cfg_train = cfg_run['cfg_train']
     cfg_export = cfg_run['cfg_export']
 
@@ -67,25 +75,49 @@ def parse_cfg_dict(cfg_run:(dict)):
         raise ValueError(f"Unknown/Unsupported device: {device_str}. Choose one of: 'cuda', 'cpu'")
     cfg_train['device'] = torch.device(device_str)
 
-    return cfg_data, cfg_train, cfg_export, cfg_critic_setup, cfg_critictrain_setup
+    return cfg_import, cfg_loader, cfg_train, cfg_export, cfg_critic_setup, cfg_critictrain_setup
+
+def unmix_script(cfg_export_master:(dict), cfg_unmix:(dict)):
+
+    """
+    Is the main 'script' used to get the unmix metrics, this is a functionalized and regularly updated version of code in this file.
+
+    Args:
+        cfg_export_master (dict): Export configs obtained from the master cfg, this is different than the one in cfg_unmix
+        cfg_unmix (dict): Cfgs to run this specific metric, for details, check example_cfg.yaml in testing/
+
+    Returns:
+        Logs the training metrics
+    """
+
+    # Parse the cfg_unmix dict
+    cfg_import, cfg_loader, cfg_train, cfg_export, cfg_critic_setup, cfg_critictrain_setup = parse_cfg_dict(cfg_unmix)
+    logging.info("Cfgs taken in, parsed")
+
+    # Sort out the data
+    configured_data = get_data(cfg_import, cfg_loader)
+    logging.info("Data configured")
+
+    # Load the critic, optimizer, lr_scheduler, loss_fn
+    critic = load_critic(cfg_critic_setup)
+    optimizer, lr_scheduler, loss_fn = load_critictrain(cfg_critictrain_setup, critic)
+    logging.info("Critic, optimizer, lr_scheduler, loss_fn configured")
+
+    logging.info("Training function called")
+    train_critic(cfg_train, cfg_export, critic, loss_fn, optimizer, lr_scheduler, configured_data)
+    logging.info("Training finished, synthesized data tested")
 
 #endregion
 
-#region Main Loop
-
-from .loaders.loaders import *
-from .critic_train.train import train
-from .auxiliary.auxiliary import *
-from .data.data import get_data
-
-import traceback
-import logging
+#region Main Loop, discontinued
 
 if __name__ == "__main__":
 
+    from .auxiliary.auxiliary import setup_logging, setup_wandb
+
     # Get the configs
     cfg_run = cfg_from_args()
-    cfg_data, cfg_train, cfg_export, cfg_critic_setup, cfg_critictrain_setup = parse_cfg_dict(cfg_run)
+    cfg_import, cfg_loader, cfg_train, cfg_export, cfg_critic_setup, cfg_critictrain_setup = parse_cfg_dict(cfg_run)
 
     # Setup wandb and logging
     run = setup_wandb(cfg_run, cfg_export)
@@ -94,7 +126,7 @@ if __name__ == "__main__":
     logging.info("Cfgs taken in, parsed. WandB and logging configured")
 
     # Figure out data
-    configured_data = get_data(cfg_data)
+    configured_data = get_data(cfg_import, cfg_loader)
 
     logging.info("Data has been parsed and configured")
 
@@ -106,7 +138,7 @@ if __name__ == "__main__":
 
     try:
         logging.info("Training function called")
-        train(cfg_train, cfg_export, critic, loss_fn, optimizer, lr_scheduler, configured_data)
+        train_critic(cfg_train, cfg_export, critic, loss_fn, optimizer, lr_scheduler, configured_data)
         logging.info("Training finished, synthesized data tested")
 
     except Exception as e:
@@ -118,3 +150,4 @@ if __name__ == "__main__":
         run.finish()
 
 #endregion
+
