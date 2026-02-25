796570a8adc438ae851b0a354ecd795a993aa2b8
Ege Artan <103586619+enatrage@users.noreply.github.com>
2026-02-25 15:16:34 +0300

    fully completed the newest abundance sampler
    - uses a log-normal mixing model that combines bunch of log-normal mixes
    - gets a much accurate sampled pdf compared to previous methods
    - prevents ood issues for generators
    - uses KMeans means as warm-ups for the model training, highly advanced math

--- defs/synthesis/abundance_sampler.py
+++ defs/synthesis/abundance_sampler.py
@@ -33,12 +33,12 @@ class UniAbSamp(AbundanceSampler):
     def __init__(self, target_shape, seed, device):
         super().__init__(target_shape, seed, device)
 
-    def _math_logic(self, tensor):
+    def math_logic(self, tensor):
         return F.normalize(tensor, p=1, dim=1, eps=self.eps)
         
     def __call__(self):
         ab = torch.rand(size=self.target_shape, generator=self.generator, device=self.device) # Generate the abundances 
-        return self._math_logic(ab)
+        return self.math_logic(ab)
     
 class NormAbSamp(AbundanceSampler):
     """
@@ -47,13 +47,13 @@ class NormAbSamp(AbundanceSampler):
     def __init__(self, target_shape, seed, device):
         super().__init__(target_shape, seed, device)
 
-    def _math_logic(self, tensor):
+    def math_logic(self, tensor):
         tensor.abs_()
         return F.normalize(tensor, p=1, dim=1, eps=self.eps)
 
     def __call__(self):
         ab = torch.randn(size=self.target_shape, generator=self.generator, device=self.device) # Generate the abundances 
-        return self._math_logic(ab)
+        return self.math_logic(ab)
     
 class DirAbSamp(AbundanceSampler):
     """
@@ -72,69 +72,71 @@ class DirAbSamp(AbundanceSampler):
         else:
             raise ValueError(f"Unknown/Unsupported alpha input type: {type(alpha)}")
         
-    def _math_logic(self, tensor):
+    def math_logic(self, tensor):
         return F.normalize(tensor, p=1, dim=1, eps=self.eps)
         
     def __call__(self):
         ab = torch.empty(size=self.target_shape, device=self.device)
         for i, alpha in enumerate(self.alphas): ab[:,i].gamma_(concentration=alpha, concentration_1=1.0, generator=self.generator)
-        return self._math_logic(ab)
+        return self.math_logic(ab)
 
 class LogNormalAbSamp(AbundanceSampler):
     """
-    An abundance sampler that assumes log-normal distribtuions
+    An abundance sampler that assumes log-normal distribution.
     """
-    def __init__(self, target_shape, seed, device, weights, mus, log_vars, alr_bool):
+    def __init__(self, target_shape, seed, device, mix_weights:(list[float]), mus:(list[list[float]]), Ls:(list[list[list[float]]]), zero_thresh:(float)=0.0001):
         super().__init__(target_shape, seed, device)
 
-        self.weights = torch.tensor(weights, device=device, dtype=torch.float32)
-        self.mus = torch.tensor(mus, device=device, dtype=torch.float32)
-        self.log_vars = torch.tensor(log_vars, device=device, dtype=torch.float32)
+        # Store the inputs
+        self.mix_weights = torch.tensor(mix_weights, dtype=torch.float32, device=device)
+        self.mus = torch.tensor(mus, dtype=torch.float32, device=device)
 
-        self.scales = F.softplus(self.log_vars) + 1e-5
+        # Permanent params, will change this majorly later on
+        _Ls = torch.tensor(Ls, dtype=torch.float32, device=device)
 
-        self.K = self.weights.shape[0]
-        self.dim = self.mus.shape[1]
+        # Validating shapes, important we do this sanity check
+        K = len(mix_weights)
+        n_endmembers = target_shape[1]
+        assert self.mus.shape == (K, n_endmembers), f"Mus shape ({self.mus.shape}) must match the expected shape of mixtures and endmembers ({(K, n_endmembers)})"
 
-        self.alr_bool = alr_bool
+        # Get the scale trils from Ls
+        L_tril = torch.tril(_Ls)
+        diag = torch.diagonal(L_tril, dim1=-2, dim2=-1)
+        off_diag = L_tril - torch.diag_embed(diag)
+        pos_diag = F.softplus(diag) + self.eps
+        self.scale_trils = off_diag + torch.diag_embed(pos_diag)   
 
-    def _sampling_logic(self):
-        if self.alr_bool:
-            cat = torch.distributions.Categorical(self.weights)
-            ks = cat.sample((self.target_shape[0],))  # (Batch,)
+        # For the thresholding during sampling
+        self.zero_thresh = zero_thresh
 
-            # Sample in alr
-            eps = torch.randn(self.target_shape[0], self.dim, device=self.device)
-            ab = self.mus[ks] + self.scales[ks] * eps  # (Batch, n_endmember - 1)
-
-            # Get back to simplex
-            ab = self._inv_alr(ab) # (Batch, n_endmember)
-            return ab
-        else:
-            # Weird sampling
-            randn = torch.randn(size=self.target_shape, device=self.device)
-            ab = torch.exp(self.mus + self.sigmas * randn)
-            return ab 
-
-    def _inv_alr(self, ab):
+    def math_logic(self, tensor):
         """
-        Inverses the ALR.
-
-        Args:
-            ab_alr: Abundances on R^2, (..., 2)
-        Returns:
-            ab_simplex: Abundances on simplex, (..., 3)
+        First, normalizes as a sanity check, then clips the values below the thresh to 0, then normalizes again
+        We have to do this because by the math that takes place, we can mathematically never have abundances 
+        Really close to 0
         """
-        expz = torch.exp(ab)
-        denom = 1 + expz.sum(dim=-1, keepdim=True)
-        x12 = expz / denom
-        x3 = 1 / denom
-        return torch.cat([x12, x3], dim=-1)
-
-    def _math_logic(self, tensor):
+        tensor = F.normalize(tensor, p=1, dim=1, eps=self.eps)
+        mask = tensor < self.zero_thresh
+        tensor = tensor.masked_fill(mask, 0.0)
         return F.normalize(tensor, p=1, dim=1, eps=self.eps)
-
+    
     def __call__(self):
-        ab = self._sampling_logic()
-        return self._math_logic(ab)
+        """
+        Randomly creates the abundances using the learned distribution
+        """
+        batch_size, n_endmembers = self.target_shape[0], self.target_shape[1]
 
+        # We have to randomly choose one of the learned distributions, for each of the batch entries
+        selected_components = torch.multinomial( self.mix_weights, batch_size, replacement=True, generator=self.generator) # Size is (Batch,)
+        selected_mus = self.mus[selected_components] # Shape is (Batch, n_endmembers)
+        selected_trils = self.scale_trils[selected_components] # Shape is (Batch, n_endmembers, n_endmembers)
+
+        # Sample a normal dist
+        epsilon = torch.randn(batch_size, n_endmembers, 1, device=self.device, generator=self.generator)
+
+        # Perform the operation to get multivar normal in log-space:
+        # (Batch, n_endmembers, 1) = (Batch, n_endmembers, 1) + (Batch, n_endmembers, n_endmembers) @ (Batch, n_endmembers, 1)
+        log_ab = selected_mus + (selected_trils @ epsilon).squeeze(-1)
+        sampled_ab = torch.exp(log_ab)
+
+        return self.math_logic(sampled_ab)
