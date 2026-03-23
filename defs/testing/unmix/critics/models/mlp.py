498748ad4e9b878334e15e44cf08773d9619876e
Andrew Peng <andrew.peng.canada@gmail.com>
2026-03-23 15:35:31 -0400

    merging

--- defs/testing/unmix/critics/models/mlp.py
+++ defs/testing/unmix/critics/models/mlp.py
@@ -6,7 +6,11 @@ from diffusers.models.activations import get_activation
 class MLP(nn.Module):
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     def __init__(self, hidden_dim:list[int], i_dim:(int)=210, o_dim:(int)=3, dropout:(float)=0.1, leaky_slope:(float)=0.2):
+=======
+    def __init__(self, hidden_dim:list[int], i_dim:(int)=81, o_dim:(int)=3, dropout:(float)=0.1, leaky_slope:(float)=0.2):
+>>>>>>> b04a5651f926d8053e527722711cc244d89e3902
 =======
     def __init__(self, hidden_dim:list[int], i_dim:(int)=81, o_dim:(int)=3, dropout:(float)=0.1, leaky_slope:(float)=0.2):
 >>>>>>> b04a5651f926d8053e527722711cc244d89e3902
